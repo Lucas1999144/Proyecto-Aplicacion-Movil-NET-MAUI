c794eb991c99ac0e62266f45ce5e062ac033f6d3
Lucas Navarro <navarrolcs99@gmail.com>
2026-06-28 19:23:42 -0300

    Cambios por errores

--- Parcial_Moviles.Tests/obj/Debug/net9.0/Parcial_Moviles.Tests.AssemblyInfo.cs
+++ Parcial_Moviles.Tests/obj/Debug/net9.0/Parcial_Moviles.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parcial_Moviles.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c30cb142e143748ff28d51c8f3df6ead66c6667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de028dc1e0f9029187e37039ceae59eb40bfdeee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parcial_Moviles.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parcial_Moviles.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
