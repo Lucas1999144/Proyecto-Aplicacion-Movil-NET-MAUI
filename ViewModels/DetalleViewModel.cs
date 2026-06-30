810c26d8351063d366a5b44f2151cbd35a7e5cbb
Lucas Navarro <navarrolcs99@gmail.com>
2026-06-29 22:55:19 -0300

    Hice cambios dentro de la solucion sacando el Parcial_Moviles.Tests y sumando GPS mas vibracion

--- ViewModels/DetalleViewModel.cs
+++ ViewModels/DetalleViewModel.cs
@@ -1,20 +1,55 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Parcial_Moviles.Models;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Parcial_Moviles.ViewModels;
 
 [QueryProperty(nameof(Product), "product")]
 public partial class DetalleViewModel : BaseViewModel
-{    
+{
     [ObservableProperty]
     private Product? product;
 
     [ObservableProperty]
     private string title = "Detalle del Producto";
 
+    [ObservableProperty]
+    private string ubi = string.Empty; /// Muestra ubi en UI
+
     partial void OnProductChanged(Product? oldValue, Product? newValue)
     {
         if (newValue != null)
+        {
             Title = newValue.Title;
+            _ = ObtenerUbiAsync();
+        }
+    }
+
+[RelayCommand]
+    private async Task ObtenerUbiAsync()
+    {
+        try
+        {
+            var req = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+            var loc = await Geolocation.GetLocationAsync(req);
+            if (loc != null)
+            {
+                Ubi = $"Latitud: {loc.Latitude:F4}, Longitud: {loc.Longitude:F4}";
+            }
+            else
+                Ubi = "No se encontro la ubicacion";
+        }
+        catch (FeatureNotSupportedException)
+        {
+            Ubi = "GPS no disponible para este tipo de dispositivo";
+        }
+        catch (PermissionException)
+        {
+            Ubi = "Permiso de ubicacion no otorgado";
+        }
+        catch (Exception ex)
+        {
+            Ubi = $"Error: {ex.Message}";
+        }
     }
 }
